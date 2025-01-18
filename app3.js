df5e6566247b44f0bbfdf453e4d186ca2b8d1932
SaptakGit <sadcat14@gmail.com>
2025-01-18 07:40:24 +0530

    Ep-2: How Js works on Server | Ep-3: global object, window/this/self/frames - global, globalThis

---
+++ app3.js
@@ -0,0 +1,10 @@
+var name = "Namaste NodeJs";
+
+var a = 10;
+var b = 20;
+
+//console.log(name);
+//console.log(a+b);
+//console.log(global);
+console.log(this); // Empty Object
+console.log(globalThis); // Global Object
